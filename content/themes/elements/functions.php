501b19ea5c5b5f2871aaba3c1b31d80c0cb83717
Luc Awater <lucawater@gmail.com>
2017-02-04 21:35:23 +0100

    Remove all unnecessary files

--- content/themes/elements/functions.php
+++ content/themes/elements/functions.php
@@ -6,20 +6,6 @@
 // Includes
 require_once('includes/scripts.php');
 
-// Includes: admin
-require_once('includes/admin/acf-page.php');
-
-// Inludes: template functions
-require_once('includes/functions-template/posts.php');
-require_once('includes/functions-template/section.php');
-require_once('includes/functions-template/section-header.php');
-require_once('includes/functions-template/section-grid.php');
-require_once('includes/functions-template/section-slider.php');
-
-// Initialize mobile detect
-require_once('includes/mobile-detect.php');
-$detect = new Mobile_Detect;
-
 // Hide admin bar
 add_filter('show_admin_bar', '__return_false');
 
